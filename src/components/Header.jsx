858f1336703470714a1cca4ce175c14380fbbc64
adnan <sirgeant10@gmail.com>
2024-11-13 21:51:45 +0600

    Add current date

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
+import moment from "moment";
 import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-2">
       <div>
-        <img src={logo} alt="" className="w-[300px]"/>
+        <img src={logo} alt="" className="w-[300px]" />
       </div>
+      <h2 className="text-gray-400">Journalism without Fear or Favorr</h2>
+      <p>{moment().format("dddd, MMMM Do YYYY")}</p>
     </div>
   );
 };
