ec12d69f01c3d7962568a9c8ef037504c18910ec
adnan <sirgeant10@gmail.com>
2024-11-13 22:19:24 +0600

    Add skeleton for main content

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -14,7 +14,11 @@ const HomeLayout = () => {
       <nav className="w-11/12 mx-auto px-2">
         <Navbar></Navbar>
       </nav>
-      <main></main>
+      <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
+        <aside className="left col-span-3">Left Navbar</aside>
+        <section className="col-span-6">Main Content</section>
+        <aside className="col-span-3">Right Navbar</aside>
+      </main>
     </div>
   );
 };
