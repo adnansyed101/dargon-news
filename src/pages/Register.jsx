2e9214a6266af2656b847c8abbdd0be6ccd98142
adnan <sirgeant10@gmail.com>
2024-11-15 15:59:21 +0600

    Add registration functions

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,41 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
 
 const Register = () => {
+  const { createNewUser, setUser } = useContext(AuthContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const name = form.get("name");
+    const photo = form.get("photo");
+    const email = form.get("email");
+    const password = form.get("password");
+
+    console.log({ name, photo, email, password });
+
+    createNewUser(email, password)
+      .then((res) => {
+        const user = res.user;
+        setUser(user)
+        // console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMsg = error.message;
+
+        console.log(errorCode, errorMsg);
+      });
+  };
+
   return (
     <div className="min-h-screen flex justify-center items-center">
       <div className="card bg-base-100 w-full max-w-lg shrink-0 rounded-none p-10">
         <h2 className="text-2xl font-semibold text-center">
           Register your account
         </h2>
-        <form className="card-body">
+        <form onSubmit={handleSubmit} className="card-body">
           {/* Name */}
           <div className="form-control">
             <label className="label">
@@ -15,6 +43,7 @@ const Register = () => {
             </label>
             <input
               type="text"
+              name="name"
               placeholder="Name"
               className="input input-bordered"
               required
@@ -27,6 +56,7 @@ const Register = () => {
             </label>
             <input
               type="text"
+              name="photo"
               placeholder="photo-url"
               className="input input-bordered"
               required
@@ -39,6 +69,7 @@ const Register = () => {
             </label>
             <input
               type="email"
+              name="email"
               placeholder="email"
               className="input input-bordered"
               required
@@ -51,6 +82,7 @@ const Register = () => {
             </label>
             <input
               type="password"
+              name="password"
               placeholder="password"
               className="input input-bordered"
               required
