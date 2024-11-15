87479fcd135c39d4bba34552294307925bb2b803
adnan <sirgeant10@gmail.com>
2024-11-15 17:58:56 +0600

    Set location to navigate state

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import PropTypes from "prop-types";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import Loading from "../pages/Loading";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
 
   if (loading) {
     return <Loading />;
@@ -15,7 +16,7 @@ const PrivateRoute = ({ children }) => {
     return children;
   }
 
-  return <Navigate to="/auth/login" />;
+  return <Navigate state={location.pathname} to="/auth/login" />;
 };
 
 PrivateRoute.propTypes = {
